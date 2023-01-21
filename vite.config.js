c3a795a6cb5b77bb32580ffe19953527df1322a8
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-21 15:44:43 +0100

    fix: :bug: add routes (MPA style) to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 // vite.config.js
+import { resolve } from 'path'
 import { defineConfig } from "vite";
 export default defineConfig({
   // config options
-  base: "/Real-Estate-website",
+  base: "/Real-Estate-website/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about/index.html'),
+        contact: resolve(__dirname, 'contact/index.html'),
+        properties: resolve(__dirname, 'properties/index.html'),
+        sell: resolve(__dirname, 'sell/index.html'),
+        testimonials: resolve(__dirname, 'testimonials/index.html'),
+      },
+    },
+  },
 });
